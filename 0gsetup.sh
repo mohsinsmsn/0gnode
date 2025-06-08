a4899e9ab9a1534bcf48d02d509a6c2a071548ee
Mohisn Bhat <mohsinsmsn@gmail.com>
2025-06-08 20:18:26 +0530

    Update 0gsetup.sh

--- 0gsetup.sh
+++ 0gsetup.sh
@@ -14,4 +14,30 @@ git clone https://github.com/0glabs/0g-storage-node.git
 cd 0g-storage-node && git checkout v1.0.0 && git submodule update --init
 cargo build --release
 rm -rf $HOME/0g-storage-node/run/config.toml
-curl -o $HOME/0g-storage-node/run/config.toml https://raw.githubusercontent.com/Mayankgg01/0G-Storage-Node-Guide/main/config.toml
+curl -o $HOME/0g-storage-node/run/config.toml https://raw.githubusercontent.com/mohsinsmsn/0gnode/refs/heads/main/config.toml
+
+#Enter your Private Key with 0x as prefix
+nano $HOME/0g-storage-node/run/config.toml
+
+#System Service
+sudo tee /etc/systemd/system/zgs.service > /dev/null <<EOF
+[Unit]
+Description=ZGS Node
+After=network.target
+
+[Service]
+User=$USER
+WorkingDirectory=$HOME/0g-storage-node/run
+ExecStart=$HOME/0g-storage-node/target/release/zgs_node --config $HOME/0g-storage-node/run/config.toml
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable zgs
+sudo systemctl start zgs
+
